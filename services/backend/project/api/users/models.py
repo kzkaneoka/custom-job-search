40dea44f9c112cfa968aaf7518405579aaecb066
Kazuki Kaneoka <kzkaneoka@gmail.com>
2019-12-09 08:49:56 -0800

    Implemented auth token using JWT

--- services/backend/project/api/users/models.py
+++ services/backend/project/api/users/models.py
@@ -1,7 +1,9 @@
+import datetime
 import os
 
 from flask import current_app
 from sqlalchemy.sql import func
+import jwt
 
 from project import db, bcrypt
 
@@ -35,18 +37,32 @@ class User(db.Model):
         """Generates the access token"""
         try:
             payload = {
-                'exp': datetime.datetime.utcnow() + datetime.timedelta(
-                    days=0, seconds=5),
-                'iat': datetime.datetime.utcnow(),
-                'sub': user_id
+                "exp": datetime.datetime.utcnow()
+                + datetime.timedelta(
+                    days=current_app.config.get("TOKEN_EXPIRATION_DAYS"),
+                    seconds=current_app.config.get("TOKEN_EXPIRATION_SECONDS"),
+                ),
+                "iat": datetime.datetime.utcnow(),
+                "sub": user_id,
             }
             return jwt.encode(
-                payload,
-                current_app.config.get('SECRET_KEY'),
-                algorithm='HS256'
+                payload, current_app.config.get("SECRET_KEY"), algorithm="HS256"
             )
         except Exception as e:
             return e
+
+    @staticmethod
+    def decode_auth_token(auth_token):
+        """
+        Decodes the access token - :param auth_token: - :return: integer|string
+        """
+        try:
+            payload = jwt.decode(auth_token, current_app.config.get("SECRET_KEY"))
+            return payload["sub"]
+        except jwt.ExpiredSignatureError:
+            return "Signature expired. Please log in again."
+        except jwt.InvalidTokenError:
+            return "Invalid token. Please log in again."
 
 
 if os.getenv("FLASK_ENV") == "development":
