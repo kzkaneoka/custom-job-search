fae63e37eb2eb099a8a4e4aad449af253cbb806e
Kazuki Kaneoka <kzkaneoka@gmail.com>
2019-12-09 11:21:27 -0800

    Implemented navbar

---
+++ services/frontend/src/components/tests/NavBar.test.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { cleanup } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import PropTypes from "prop-types";
+
+import NavBar from "../NavBar";
+
+afterEach(cleanup);
+
+const title = "Hello, World!";
+
+it("renders a title", () => {
+  const { getByText } = renderWithRouter(<NavBar title={title} />);
+  expect(getByText(title)).toHaveClass("nav-title");
+});
+
+it("renders", () => {
+  const { asFragment } = renderWithRouter(<NavBar title={title} />);
+  expect(asFragment()).toMatchSnapshot();
+});
+
+NavBar.propTypes = {
+  title: PropTypes.string.isRequired
+};
